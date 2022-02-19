38046c5aaf991c78f9cfd11b96318c2debecaac4
Yurii <juracherniak@gmail.com>
2022-02-19 13:05:11 +0100

    add publish sort

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
     <React.StrictMode>
-        <Router basename="/">
+        <Router basename="/blog_page_client">
             <App />
         </Router>
     </React.StrictMode>,
--- src/pages/Posts.js
+++ src/pages/Posts.js
@@ -6,25 +6,28 @@ export const Posts = ({ posts }) => {
     return (
         <>
             <section className="hi-section">
-                
-                    <header>{"<Hello World! />"}</header>
-                
+                <header>{"<Hello World! />"}</header>
+
                 <p>My name is Yurii Cherniak. Welcome to my blog!</p>
             </section>
             <div className="posts-grid">
                 {posts &&
                     posts.map((post) => {
-                        return (
-                            <Post
-                                key={post._id}
-                                id={post._id}
-                                title={post.title}
-                                text={post.text}
-                                date={post.date}
-                                author={post.author}
-                                comments={post.comments}
-                            />
-                        );
+                        if (post.published) {
+                            return (
+                                <Post
+                                    key={post._id}
+                                    id={post._id}
+                                    title={post.title}
+                                    text={post.text}
+                                    date={post.date}
+                                    author={post.author}
+                                    comments={post.comments}
+                                />
+                            );
+                        } else {
+                            return null;
+                        }
                     })}
             </div>
         </>
